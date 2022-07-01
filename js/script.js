df077c49c37b4d2ac2abb86a8b4dedd6842cad05
feddericahu99 <federica.hu339@gmail.com>
2022-07-01 15:57:56 +0200

    random number function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,109 @@
+/*
+ Consegna:
+    Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco 
+    Il computer deve generare 16 numeri casuali nello stesso range della 
+    difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere 
+    posizionata al massimo una bomba, 
+    perciò nell’array delle bombe non potranno esserci due numeri uguali.
+    In seguito l'utente clicca su una cella: se il numero è presente nella 
+    lista dei numeri generati (delle bombe) - abbiamo calpestato una bomba - 
+    la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata 
+    si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+    La partita termina quando il giocatore clicca su una bomba o quando raggiunge il 
+    numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte 
+    le celle che non sono bombe).
+
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di 
+volte che l’utente ha cliccato su una cella che non era una bomba.
+
+# MILESTONE 1
+Prepariamo "qualcosa" per tenere il punteggio dell'utente.
+Quando l'utente clicca su una cella, incrementiamo il punteggio.
+Se riusciamo, facciamo anche in modo da non poter più cliccare la stessa cella.
+
+# MILESTONE 2
+Facciamo in modo di generare 16 numeri casuali (tutti diversi) compresi tra 1 e 
+il massimo di caselle disponibili.
+Generiamoli e stampiamo in console per essere certi che siano corretti.
+
+# MILESTONE 3
+Quando l'utente clicca su una cella, verifichiamo se ha calpestato una bomba, 
+controllando se il numero di cella è presente nell'array di bombe. Se si, 
+la cella diventa rossa (raccogliamo il punteggio e e scriviamo in console che 
+    la partita termina) altrimenti diventa azzurra e dobbiamo incrementare il punteggio.
+
+# MILESTONE 4
+Quando l'utente clicca su una cella, e questa non è una bomba, dobbiamo controllare 
+se il punteggio incrementato ha raggiunto il punteggio massimo perchè in quel caso la 
+partita termina. Raccogliamo quindi il messaggio è scriviamo un messaggio appropriato.
+(Ma come stabiliamo quale sia il punteggio massimo?)
+
+# MILESTONE 5
+Quando la partita termina dobbiamo capire se è terminata perchè è stata cliccata una bomba 
+o se perchè l'utente ha raggiunto il punteggio massimo. Dobbiamo poi stampare in pagina il 
+punteggio raggiunto ed il messaggio adeguato in caso di vittoria o sconfitta.
+
+*/
+
 //prendo i miei elementi
 const btn = document.getElementById('start-game'); 
 let game = document.getElementById('game');
+const bomb= [];
+console.table(bomb)
+const score = [];
+console.table(score)
 
 // functions
+
+// randomizzatore
+const getRandomNumber =  (number)  => {
+    let i= 1;
+    while ( i <= number ){
+        let randomic= Math.floor(Math.random()* 100 + 1 -1) +1; 
+        i++;
+        if ( randomic === randomic) {
+            return
+        }
+        console.log(randomic)
+
+    }
+    return 
+}
+
+// creazione griglie
 const createGrid = (row= 10, col=10 ) => {
-   const totalCell= row * col;
+    const totalCell= row * col;
     let number= 1;
 
     // ripeto per il risultato di totalCell volte, 1 per ogni griglia del gioco
     for( i = 1; i <= totalCell; i++) {
 
-      // creo il div all'interno del html e gli aggiungo la classe
-      let cell = document.createElement('div');
-      cell.classList.add('cell')
-   
-      // porto nel dom
-      game.appendChild(cell)
-      cell.innerText = number;
+        // creo il div all'interno del html e gli aggiungo la classe
+        const cell = document.createElement('div');
+        cell.classList.add('cell')
+    
+        // porto nel dom
+        game.appendChild(cell)
+        cell.innerText = parseInt(number);
 
-      //incremento
-      number++;
+        //incremento
+        number++;
 
-      //azione per colorare di blu cliccano sulla cella 
-      cell.addEventListener('click', (event) => {
-      event.target.classList.toggle('clicked')
-      console.log(event.target.innerText)
-    });
-       
+        //azione per colorare di blu cliccano sulla cella 
+        cell.addEventListener('click', (event) => {
+            let cellNumber = event.target.innerText;
+            console.log(cellNumber);
+            score.push(cellNumber);
+
+            //blocco celle già premute
+            if (event.target.classList.contains('clicked')){
+                return
+            }
+            //aggiungo classe ai selezionati    
+            event.target.classList.add('clicked')
+            console.log(event.target.innerText)
+            });
+        
    }
 }
 
@@ -35,8 +112,8 @@ const createGrid = (row= 10, col=10 ) => {
 //azione al click del bottone 
 btn.addEventListener('click',() => {       
     // creo griglie
-    grid = createGrid()
-    
+    grid = createGrid();
+    randomizer = getRandomNumber(16);
 })
 
 
